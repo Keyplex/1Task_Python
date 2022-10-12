68df3a87c51fe3f5faa8b14d6d4772f54da390ab
Pavel <51826654+1commando1@users.noreply.github.com>
2022-10-12 18:00:27 +0300

    .

---
+++ 2 поток/5.Реализовать_структуру.py
@@ -0,0 +1,22 @@
+# 5. Реализовать структуру «Рейтинг», представляющую собой
+# не возрастающий набор натуральных чисел.
+# У пользователя необходимо запрашивать новый элемент рейтинга.
+# Если в рейтинге существуют элементы с одинаковыми значениями,
+# то новый элемент с тем же значением должен разместиться после них.
+
+list = [5, 4, 3, 2, 1]
+print(f"Рейтинг - {list}")
+digit = int(input("Введите число (000 - выход) "))
+while digit != 000:
+    for el in range(len(list)):
+        if list[el] == digit:
+            list.insert(el + 1, digit)
+            break
+        elif list[0] < digit:
+            list.insert(0, digit)
+        elif list[-1] > digit:
+            list.append(digit)
+        elif list[el] > digit and list[el + 1] < digit:
+            list.insert(el + 1, digit)
+    print(f"Текущий список - {list}")
+    digit = int(input("Введите число (000 - выход) "))
