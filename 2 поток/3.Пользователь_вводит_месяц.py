2f1932087ea1586e39e05243cd0172a7f81bae76
Pavel <51826654+1commando1@users.noreply.github.com>
2022-10-13 11:29:50 +0300

    Обновили

--- 2 поток/3.Пользователь_вводит_месяц.py
+++ 2 поток/3.Пользователь_вводит_месяц.py
@@ -3,20 +3,46 @@
 # Напишите решения через list и через dict.
 
 _list = ['Зима', 'Весна', 'Лето', 'Осень']
-_dict = {1 : 'Зима', 2 : 'Весна', 3 : 'Лето', 4 : 'Осень'}
-month = int(input("Введите месяц по номеру "))
-if month == 12 or month == 1 or month == 2:
-        print(_dict.get(1))
-        print(_list[0])
-elif month == 3 or month == 4 or month ==5:
-    print(_dict.get(2))
+_dict = {12 : 'Декабрь', 1 : 'Январь', 2 : 'Февраль', 
+        3 : 'Март', 4 : 'Апрель', 5 : 'Май', 
+        6 : 'Июнь', 7 : 'Июль', 8 : 'Август', 
+        9 : 'Сентябрь', 10 : 'Октябрь', 11 : 'Ноябрь'}
+month = int(input("Введите месяц по номеру: "))
+if month == 12:           
+    print(_list[0])
+    print(_dict.get(12)) 
+elif month == 1:          
+    print(_list[0])
+    print(_dict.get(1))  
+elif month == 2:
+    print(_list[0])
+    print(_dict.get(2)) 
+elif month == 3:
     print(_list[1])
-elif month == 6 or month == 7 or month == 8:
-    print(_dict.get(3))
+    print(_dict.get(3))  
+elif month == 4:
+    print(_list[1])
+    print(_dict.get(4))  
+elif month == 5:
+    print(_list[1])
+    print(_dict.get(5))  
+elif month == 6:
     print(_list[2])
-
-elif month == 9 or month == 10 or month == 11:
-    print(_dict.get(4))
+    print(_dict.get(6))  
+elif month == 7:
+    print(_list[2])
+    print(_dict.get(7))  
+elif month == 8:
+    print(_list[2])
+    print(_dict.get(8))  
+elif month == 9:
     print(_list[3])
+    print(_dict.get(9)) 
+elif month == 10:
+    print(_list[3])
+    print(_dict.get(10)) 
+elif month == 11:
+    print(_list[3])
+    print(_dict.get(11)) 
 else:
-        print("Такого месяца не существует")
+    print("Такого месяца не существует")
