803f2da241d33bf339bcbcdeab95fd7b1ec36ae0
Pavel <51826654+1commando1@users.noreply.github.com>
2022-10-13 10:20:48 +0300

    .

--- 3 поток/5.Программа_пользователь_строку_чисел.py
+++ 3 поток/5.Программа_пользователь_строку_чисел.py
@@ -7,3 +7,20 @@
 # то вначале нужно добавить сумму этих чисел к полученной ранее сумме
 # и после этого завершить программу.
 
+def sum ():
+    sum_res = 0
+    ex = False
+    while ex == False:
+        number = input('Введите числа или Q для выхода -').split()
+
+        res = 0
+        for el in range(len(number)):
+            if number[el] == 'q' or number[el] == 'Q':
+                ex = True
+                break
+            else:
+                res = res + int(number[el])
+        sum_res = sum_res + res
+        print(f'Текущая сумма составляет: {sum_res}')
+    print(f'В итоге сумма: {sum_res}')
+sum() 
